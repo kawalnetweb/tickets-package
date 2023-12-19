1541911307b0fafe3ee0d98b6e3f328848dde786
ankitcoding1990 <ankitcoding1990@gmail.com>
2023-12-19 16:41:36 +0530

    url changes

--- src/config/tickets.php
+++ src/config/tickets.php
@@ -2,7 +2,7 @@
 return [
     'client_token' => env('CLIENT_TOKEN'),
     'project_code' => env('PROJECT_CODE'),
-    'api_url' => env('API_URL','https://crm.ixoraeducation.com/api'),
+    'api_url' => env('API_URL','https://crm.ixoraeducation.com/tickets/api'),
     'sftp' => [
         'host' => env('TICKET_SFTP_HOST'),
         'username' => env('TICKET_SFTP_USERNAME'),
